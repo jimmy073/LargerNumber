27999ef4d151322c435276d48796d00639a761a1
jimmy073 <irakizajimmy1@gmail.com>
2020-08-24 15:06:29 +0200

    Added greeting mesaage in largerNumber class

--- src/lagernbr/Lagernbr.java
+++ src/lagernbr/Lagernbr.java
@@ -15,7 +15,7 @@ public class Lagernbr {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       system.out.printl("Hello Java developer");
     }
     
 }
